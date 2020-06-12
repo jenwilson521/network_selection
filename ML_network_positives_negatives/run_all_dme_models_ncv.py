293a3bd3d6d30a2111f71817959f44bdd90094f7
Jennifer Lynn Wilson <wilsonjl@shahlab-dev2.stanford.edu>
2020-06-12 11:49:56 -0700

    new figures and excel output

--- ML_network_positives_negatives/run_all_dme_models_ncv.py
+++ ML_network_positives_negatives/run_all_dme_models_ncv.py
@@ -6,6 +6,7 @@ import pickle,os,csv,matplotlib
 matplotlib.use("AGG")
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 allf = [f for f in os.listdir('.') if 'dme_' in f and '.txt' in f]
 all_dmes = sorted([x.replace('dme_','').replace('.txt','') for x in allf])
@@ -30,7 +31,7 @@ for model_type in ['log_reg','dec_tree']:
 		if tp > 10 and tn > 10:
 			cmd = 'python all_pathways_with_validation.py %s -m %s -n %s -v %s -s %s'%(f,model_type,dme,'nest',val_type)
 			print(cmd)
-			os.system(cmd)
+#			os.system(cmd)
 
 		else:
 			print('skipped')
@@ -75,25 +76,52 @@ plt.close()
 
 
 ## plot feature importance
+outf = 'log_reg_coeff_vs_dec_tree_feat_imp.xlsx'
+writer = pd.ExcelWriter(outf)
+ord_col = ['name','dec tree feat imp','dec tree err','log reg coeff','log reg err']
 allf = [f for f in allf if 'log_reg' in f]
 for f in allf:
 	dme = f.split("_log_reg")[0]
 	lr = pickle.load(open(os.path.join('nestedcv_nest',f),'rb'))
 	dt = pickle.load(open(os.path.join('nestedcv_nest',f.replace("log_reg", "dec_tree")),'rb'))
 
-	xlabel = [l[0] for l in lr]
-	lr_coef_ = [[l[1] for l in lr],
-				[l[2] for l in lr]]
-	dt_feat_imp = [[d[1] for d in dt],
-				[d[2] for d in dt]]
+	lr_fi = dict([(nm,(fval,ferr)) for (nm,fval,ferr) in lr['features_importance']])
+	dt_fi = dict([(nm,(fval,ferr)) for (nm,fval,ferr) in dt['features_importance']])
+	xlabel = sorted([k for k in lr_fi.keys()])
+
+	lr_coef_ = np.zeros((2,len(xlabel)))
+	dt_coef_ = np.zeros((2,len(xlabel)))
+	row_data = []
+	# make sure ordering of features is the same
+	for (i,xl) in enumerate(xlabel):
+		rd = {}
+		lr_coef_[0][i] = lr_fi[xl][0] 
+		lr_coef_[1][i] = lr_fi[xl][1]
+		dt_coef_[0][i] = dt_fi[xl][0]
+		dt_coef_[1][i] = dt_fi[xl][1]
+		rd['name'] = xl
+		rd['log reg coeff'] = lr_fi[xl][0]
+		rd['log reg err'] = lr_fi[xl][1]
+		rd['dec tree feat imp'] = dt_fi[xl][0]
+		rd['dec tree err'] = dt_fi[xl][1]
+		row_data.append(rd)
+	df = pd.DataFrame(row_data)
+	df = df[ord_col].sort_values(by=['dec tree feat imp'],ascending=False)
+	df.to_excel(writer,sheet_name = dme)
+
+#	lr_coef_ = [[l[1] for l in lr_fi],
+#				[l[2] for l in lr_fi]]
+#	dt_fi = dt['features_importance']
+#	dt_feat_imp = [[d[1] for d in dt_fi],
+#				[d[2] for d in dt_fi]]
 
 	# Setting the positions and width for bars
-	pos = list(range(len(label))) 
+	pos = list(range(len(xlabel))) 
 	width = 0.25
 	# Plotting the bars
 	fig,ax_arr = plt.subplots(2,1,sharex=True,figsize=(5,7.5))
 
-	for i, (value, color, l, ylabel) in enumerate([(lr_coef_, "green", "log_reg", "coefficent value"), (dt_feat_imp, "red", "det_tree", "feature importance")]):
+	for i, (value, color, l, ylabel) in enumerate([(lr_coef_, "green", "log_reg", "coefficent value"), (dt_coef_, "red", "det_tree", "feature importance")]):
 		ax = ax_arr[i]
 		rects = ax.bar(pos, value[0], width, 
 					   alpha=0.5, color=color, yerr = value[1]) ## to remove error bars just comment yerr
@@ -109,5 +137,5 @@ for f in allf:
 	plt.tight_layout()
 	fig.savefig("histogram_" + dme + ".png")
 	plt.close(fig)
-
+writer.close()
 
